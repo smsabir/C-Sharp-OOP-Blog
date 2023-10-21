99b58465c153e8c69e1d9ecb4ef9ccd5dc07c773
smsabir <sabir.smaa2@gmail.com>
2023-10-21 03:50:25 +0300

    Class Lesson 1

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using OOP;
+
+Vehicle myVehicle = new Vehicle("S M", "Sabir");
+
+myVehicle.Brand = "Volvo";
+myVehicle.Type = "SUV";
+myVehicle.Year = 2021;
+myVehicle.Model = "XC90";
+myVehicle.FuelType = "Petrol";
+
+Console.WriteLine("Brand: {0} \nYear: {1}", myVehicle.Brand, myVehicle.Year);
+
+//A way of getting the properties and printing them
+PropertyInfo [] properties = myVehicle.GetType().GetProperties();
+foreach(PropertyInfo p in properties)
+{
+    System.Console.WriteLine($"{p.Name} : {p.GetValue(myVehicle)}");
+}
---
+++ OOP/Vehicle.cs
@@ -0,0 +1,53 @@
+﻿namespace OOP;
+
+public class Vehicle
+{
+    public string Brand;
+    private string _type;
+    // Field with specified default value.
+    public int Year = 2023;
+    private string _userFirstName;
+    private string _userLastName;
+    string _fuelType;
+
+    //Constructor
+    public Vehicle(string userFirstName, string userLastName)
+    {
+        _userFirstName = userFirstName;
+        _userLastName = userLastName;
+    }
+
+    // A property with logics
+    public string Type
+    {
+        get
+        {
+            if (_type == null)
+            {
+                return string.Empty;
+            }
+            else
+                return _type;
+        }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                _type = "Car";
+            else
+                _type = value;
+        }
+    }
+
+    // Auto-implemented property
+    public string Model { get; set; }
+
+    //Property with Expression body
+    public string UserName => $"{_userFirstName} {_userLastName}";
+
+    //With Expression body we can get and set value
+    public string FuelType
+    {
+        get => _fuelType;
+        set => _fuelType = value;
+    }
+}
